b9004456197a23540703d3c3c72a7b7149fee7df
cuich <cuich@DESKTOP-7UV4N74>
2017-02-13 10:33:40 +0800

    添加项目文件。

---
+++ eCharts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eCharts.MvcApplication" Language="C#" %>
---
+++ eCharts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eCharts.Startup))]
+namespace eCharts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
